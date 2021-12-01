62487e8f30561462cb7b54dccd7f700115eb9203
Amy <amy_chan@student.uml.edu>
2021-12-01 02:23:35 -0500

    window 2 with working pause/run and exit

--- test.py
+++ test.py
@@ -31,7 +31,7 @@ def make_win1():
 
     left_col1 = [[sg.Frame(' Mode ', mode_frame, element_justification='center', font='Any 24', pad=10)],
             [sg.Frame(' Duration ', duration_frame, font='Any 24', pad=10)],
-            [sg.Button(' Submit ', font='Any 20', pad=10)]]
+            [sg.Button(' Submit ', key='-SUBMIT-', font='Any 20', pad=10)]]
 
     right_col1 = [[sg.Frame(' Movement Timer ', movement_frame, element_justification='center', font='Any 24', pad=10)],
         [sg.Frame(' Score ', score_frame, element_justification='center', font='Any 24', pad=10)],
@@ -45,16 +45,16 @@ def make_win1():
 # Second window
 def make_win2():
     # Druation timer
-    remaining_duration_frame = [[sg.Text('test', key='-REMAININGDURATION-', pad=10, font='Any 20')]]
+    remaining_duration_frame = [[sg.Text('test', key='-REMAINING-DURATION-', pad=10, font='Any 20')]]
 
     # Movement Timer
-    remaining_movement_frame = [[sg.Text('test', key='-REMAININGTIMER-', pad=10, font='Any 20')]]
+    remaining_movement_frame = [[sg.Text('test', key='-REMAINING-TIMER-', pad=10, font='Any 20')]]
 
     # End Score
-    end_score_frame = [[sg.Text('test', key='-REMAININGTIMER-', pad=10, font='Any 20')]]
+    end_score_frame = [[sg.Text('test', key='-REMAINING-TIMER-', pad=10, font='Any 20')]]
 
     left_col2 = [[sg.Frame(' Remaining Time ', remaining_duration_frame, element_justification='center', font='Any 24', pad=10)],
-            [sg.Button(' Pause ', font='Any 20', pad=10)]]
+            [sg.Button(' Pause ', key='-RUN-PAUSE-', font='Any 20', pad=10)]]
 
     right_col2 = [[sg.Frame(' Movement Timer ', remaining_movement_frame, element_justification='center', font='Any 24', pad=10)],
             [sg.Button(' Exit ', font='Any 20', pad=10)]]
@@ -70,23 +70,51 @@ window1, window2 = make_win1(), None        # start off with 1 window open
 # window1.Maximize()
 current_time, paused_time, paused = 0, 0, False
 start_time = time_as_int()
+win2_active = False
 
-# Event Loop
 while True:
     window, event, values = sg.read_all_windows()
+    # --------- Window Exit --------
     if event == sg.WIN_CLOSED or event == ' Exit ':
-        window.close()
-        if window == window2:       # if closing win 2, mark as closed
-            window2 = None
-        elif window == window1:     # if closing win 1, exit program
-            break
-    # elif event == ' Submit ' and values['-MODE-'] == 'Chair' and not window2:
-    #     sg.popup('You Chose Chair Mode',
-    #              'Place a chair on the floor pad.')
-    elif event == ' Submit ' and not window2:
+        window.close()    # if closing win 1, exit program
+        break
+    elif event == '-SUBMIT-':
+        if values['-MODE-'] == 'Chair':
+            sg.popup('You Chose Chair Mode',
+                  'Place a chair on the floor pad.')
         print(values)
         window2 = make_win2()
+        win2_active = True
         # window2.Maximize()
-    # elif event == ' Erase ':
-    #     window['-IN-'].update('')
+    elif event == '-RUN-PAUSE-':
+        paused = not paused
+        if paused:
+            paused_time = time_as_int()
+        else:
+            start_time = start_time + time_as_int() - paused_time
+        window['-RUN-PAUSE-'].update(' Run ' if paused else ' Pause ')
+    
+    while win2_active:
+        event, values = window2.read(timeout=50)
+        # if not paused:
+        #     event, values = window.read(timeout=10)
+        #     current_time = time_as_int() - start_time
+        # else:
+        #     event, values = window2.read()
+        
+        if event == sg.WIN_CLOSED or event == ' Exit ':
+            window2.close()
+            print('exiting')
+            win2_active = False
+            window2 = None
+
+        elif event == '-RUN-PAUSE-':
+            paused = not paused
+            if paused:
+                paused_time = time_as_int()
+            else:
+                start_time = start_time + time_as_int() - paused_time
+            window2['-RUN-PAUSE-'].update(' Run ' if paused else ' Pause ')
+    
+    
 window.close()
