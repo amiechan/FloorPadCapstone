6bde3a5ac3d8a45ea2ae1b396f0e170c38471da3
unknown <amy_chan@student.uml.edu>
2022-01-11 19:51:57 -0500

    technical demo updated code

--- gpiotest.py
+++ gpiotest.py
@@ -3,33 +3,8 @@ from gpiozero.pins.pigpio import PiGPIOFactory
 import os
 import random
 import time
-factory = PiGPIOFactory(host='192.168.50.30')
+factory = PiGPIOFactory(host='10.0.0.22')
 
 led0 = LED(20, pin_factory=factory)
-led1 = LED(19, pin_factory=factory)
-led2 = LED(26, pin_factory=factory)
 
-button0 = Button(5, pin_factory=factory)
-button1 = Button(6, pin_factory=factory)
-button2 = Button(13, pin_factory=factory)
-
-leds = [led0, led1, led2]
-pad = {led0: button0,
-       led1: button1,
-       led2: button2}
-
-previous = None
-
-current_led = random.choice(leds)
-
-while True:
-    if current_led != previous:
-        current_led.on()
-        if (pad[current_led].is_pressed):
-            print("pressed" + str(current_led.pin))
-            current_led.off()
-            previous = current_led
-            current_led = random.choice(leds)
-    else:
-        current_led = random.choice(leds)
-
+led0.blink(on_time=1, off_time=1, n=5, background=False)
--- test.py
+++ test.py
@@ -3,22 +3,37 @@ import time
 from gpiozero import LED, Button
 from gpiozero.pins.pigpio import PiGPIOFactory
 import random
+import pygame
 
-factory = PiGPIOFactory(host='192.168.50.30')
+factory = PiGPIOFactory(host='10.0.0.22')
 
-led0 = LED(20, pin_factory=factory)
-led1 = LED(19, pin_factory=factory)
-led2 = LED(26, pin_factory=factory)
+led0 = LED(18, pin_factory=factory)
+led1 = LED(23, pin_factory=factory)
+led2 = LED(24, pin_factory=factory)
+led3 = LED(25, pin_factory=factory)
+led4 = LED(12, pin_factory=factory)
 
-button0 = Button(5, pin_factory=factory)
-button1 = Button(6, pin_factory=factory)
-button2 = Button(13, pin_factory=factory)
+correct_indicator = LED(21, pin_factory=factory)
+pygame.init()
+beep_effect = pygame.mixer.Sound('media_beep1.ogg')
 
-leds = [led0, led1, led2]
-pad = {led0: button0,
+button0 = Button(4, bounce_time=0.3, pin_factory=factory)
+button1 = Button(17, bounce_time=0.3, pin_factory=factory)
+button2 = Button(27, bounce_time=0.3, pin_factory=factory)
+button3 = Button(22, bounce_time=0.3, pin_factory=factory)
+button4 = Button(5, bounce_time=0.3, pin_factory=factory)
+
+standing_leds = [led0, led1, led2, led3, led4]
+standing_pad = {led0: button0,
+       led1: button1,
+       led2: button2,
+       led3: button3,
+       led4: button4}
+
+chair_leds = [led0, led1, led2]
+chair_pad = {led0: button0,
        led1: button1,
        led2: button2}
-
 
 sg.theme('GreenTan')
 
@@ -47,8 +62,7 @@ def make_win1():
         duration_right_col, element_justification='center', pad=10)]]
 
     # Score
-    score_frame = [[sg.Checkbox(
-        'Display Score', default=False, pad=38, key='-SCORE-', font='Any 20')]]
+    score_frame = [[sg.Checkbox('Display Score', default=False, pad=38, key='-SCORE-', font='Any 20')]]
 
     left_col1 = [[sg.Frame(' Mode ', mode_frame, element_justification='center', font='Any 24', pad=10)],
                  [sg.Frame(' Duration ', duration_frame,
@@ -88,8 +102,8 @@ def make_win2():
     layout = [[sg.Text('Session In Progress', font=(any, 30), key='-TITLE-', pad=5)],
               [sg.Column(left_col2, element_justification='center'),
                sg.Column(right_col2, element_justification='center')],
-              [sg.Frame(' Score ', end_score_frame, element_justification='center', font='Any 24', pad=10)],
-              [sg.Button(' Continue ', key='-CONTINUE-', font='Any 20', pad=10)]]
+              [sg.Frame(' Score ', end_score_frame, key='-SCORE-FRAME-', element_justification='center', font='Any 24', pad=10, visible=False)],
+              [sg.Button(' Continue ', key='-CONTINUE-', font='Any 20', pad=10, visible=False)]]
     return sg.Window('Floor Pad', layout, size=(800, 480), element_justification="center", finalize=True)
 
 # start off with 1 window open
@@ -107,9 +121,14 @@ while True:
         window1.close()
         break
     elif event == '-SUBMIT-':
-        # if values['-MODE-'] == 'Chair':
-        #     sg.popup('You Chose Chair Mode',
-        #              'Place a chair on the floor pad.')
+        if values['-MODE-'] == 'Chair':
+            print("Chair mode selected")
+            pad = chair_pad
+            leds = chair_leds
+        else:
+            print("Standing mode selected")
+            pad = standing_pad
+            leds = standing_leds
 
         # Timers in seconds
         mins_duration = values['-MINUTES-']
@@ -123,13 +142,12 @@ while True:
         session_end = False
         win2_active = True
         # window2.Maximize()
-
-    previous = None
+    
+    score_enabled = values['-SCORE-']
+    previous_led = None
     current_led = random.choice(leds)
-    ms_count = 0
     score = 0
     
-
     # window2
     while win2_active:
         # --------- Read and update window --------
@@ -149,6 +167,7 @@ while True:
             window2.close()
             print('EXITING SESSION WINDOW')
             win2_active = False
+            current_led.off()
             break
         elif event == '-RUN-PAUSE-':
             paused = not paused
@@ -159,41 +178,19 @@ while True:
                 m_start_time = m_start_time + time_as_int() - paused_time
             # Change Button Text
             window2['-RUN-PAUSE-'].update(' Run ' if paused else ' Pause ')
-
-        # Movement Timer Complete
-        if movement_timer == 0:
-            print('Movement Timer Complete')
-            # Session Complete: Movement timer greater than remaining duration
-            if movement_value > duration_timer:
-                paused = True
-                session_end = True
-                current_led.off()
-                window2['-TITLE-'].update('Session Complete')
-                window2['-RUN-PAUSE-'].update(disabled = True)
-                window2['-END-'].update(disabled = True)
-                print('ENDING SESSION: movement timer greater than remaining duration')
-            # Movement timer less than or equal to remaining duration -> Reset movement timer and continue
-            else:
-                print("reset movement timer")
-                movement_timer = movement_value
-                m_start_time = time_as_int() 
-        # Session Complete: duration timer ran out
-        elif duration_timer == 0:
-            paused = True
-            session_end = True
-            current_led.off()
-            window2['-TITLE-'].update('Session Complete')
-            window2['-RUN-PAUSE-'].update(disabled = True)
-            window2['-END-'].update(disabled = True)
-            print('ENDING SESSION: duration timer ran out')
-            
-        if current_led != previous:
+        
+        if score_enabled:
+            window2['-SCORE-FRAME-'].update(visible=True)
+        
+        if current_led != previous_led:
             current_led.on()
-            # correct button pressed
+            # correct button pressed    ``
             if (pad[current_led].is_pressed):
                 print("pressed" + str(current_led.pin))
+                beep_effect.play()
+                correct_indicator.blink(on_time=0.25, off_time=0.25, n=3, background=True)
                 current_led.off()
-                previous = current_led
+                previous_led = current_led
                 current_led = random.choice(leds)
                 score += 1
                 window2['-CURRENT-SCORE-'].update(score)
@@ -202,11 +199,41 @@ while True:
         else:
             current_led = random.choice(leds)
             movement_timer = movement_value
+
+        # Movement Timer Complete / out of time for current move
+        if movement_timer == 0:
+            print('Movement Timer Complete')
+            # Session Complete: Movement timer greater than remaining duration
+            # if movement_value > duration_timer:
+            #     paused = True
+            #     session_end = True
+            #     current_led.off()
+            #     window2['-TITLE-'].update('Session Complete')
+            #     window2['-RUN-PAUSE-'].update(disabled = True)
+            #     window2['-END-'].update(disabled = True)
+            #     print('ENDING SESSION: movement timer greater than remaining duration')
+            # # Movement timer less than or equal to remaining duration -> Reset movement timer and continue
+            # else:
+            print("Resetting movement timer")
+            movement_timer = movement_value
+            m_start_time = time_as_int() 
+            previous_led = current_led
+            current_led.off()
+            current_led = random.choice(leds)
+
+        # Session Complete: duration timer ran out
+        elif duration_timer == 0:
+            current_led.off()
+            paused = True
+            session_end = True
+            window2['-TITLE-'].update('Session Complete')
+            window2['-RUN-PAUSE-'].update(disabled = True)
+            window2['-END-'].update(disabled = True)
+            window2['-CONTINUE-'].update(visible=True)
+            print('ENDING SESSION: duration timer ran out')
             
+        # update timer displays
         window2['-REMAINING-DURATION-'].update('{:02d}:{:02d}'.format(duration_timer // 60, duration_timer % 60))
         window2['-REMAINING-TIMER-'].update('{:02d}:{:02d}'.format(movement_timer // 60, movement_timer % 60))
 
 window.close()
-
-# todo: 
-# current led stays on after exiting window2 / when script ends
